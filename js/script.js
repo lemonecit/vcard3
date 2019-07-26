e3a1adfef927631cd4ecec13abe2bbbd07ba1adb
lemonec <pierre.lemonec@gmail.com>
2019-07-26 19:25:58 +0300

    aasdasd

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+
+var socialPro = document.getElementsByClassName("this_socialLinks");
+
+
+
+document.getElementById("enable_socialLinks").addEventListener("click", function(){
+  document.getElementsByClassName('container_overlay')[0].style.visibility = 'visible';
+
+});
+
+
+
+document.getElementById("close_Window").addEventListener("click", function(){
+  document.getElementsByClassName('container_overlay')[0].style.visibility = 'hidden';
+
+});
