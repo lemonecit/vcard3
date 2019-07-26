538809cfa06a803b5b19a7117d6b48a75815493e
lemonec <pierre.lemonec@gmail.com>
2019-07-26 22:20:40 +0300

    gffgdfg

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
 
-window.onload= function (){
-  // var socialPro = document.getElementsByClassName("this_socialLinks");
-
-
-
-  document.getElementById("enable_socialLinks").addEventListener("click", function(){
-    document.getElementsByClassName('container_overlay')[0].style.visibility = 'visible';
-
-  });
 
 
 
 
-      document.getElementById("closeWindow").addEventListener("click", function(){
-        document.getElementsByClassName('container_overlay')[0].style.visibility = 'hidden';
+ function onLoadApp() {
 
-      });
-  
+   document.getElementById("enable_socialLinks").addEventListener("click", function(){
+     document.getElementsByClassName('container_overlay')[0].style.visibility = 'visible';
+
+   });
+
+
+
+
+       document.getElementById("closeWindow").addEventListener("click", function(){
+         document.getElementsByClassName('container_overlay')[0].style.visibility = 'hidden';
+
+       });
+
+
+ }
+
+
+
+
+
+
 
 
 
