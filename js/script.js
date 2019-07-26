98903dbdac3d78e610e8076c469afdb49ee3cd1e
lemonec <pierre.lemonec@gmail.com>
2019-07-26 22:32:15 +0300

    dsfdsf

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
 
 
 
+var element = document.createElement("div");
+element.id = 'testqq';
+var el = document.getElementById('testqq'); //
 
-$(document).ready(function() {
-    //ente   document.getElementById("enable_socialLinks").addEventListener("click", function(){
-         document.getElementsByClassName('container_overlay')[0].style.visibility = 'visible';
+
+
+   document.getElementById("enable_socialLinks").addEventListener("click", function(){
+     document.getElementsByClassName('container_overlay')[0].style.visibility = 'visible';
+
+   });
+
+
+   var element = document.createElement("div");
+   element.id = 'closeWindow';
+   var el = document.getElementById('testqq'); //
+
+      el.addEventListener("click", function(){
+         document.getElementsByClassName('container_overlay')[0].style.visibility = 'hidden';
 
        });
-
-
-
-
-           document.getElementById("closeWindow").addEventListener("click", function(){
-             document.getElementsByClassName('container_overlay')[0].style.visibility = 'hidden';
-
-           });
-
-});
