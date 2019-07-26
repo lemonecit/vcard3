6b5d701d39e2cd8b58941cf8514e3cb3479912da
lemonec <pierre.lemonec@gmail.com>
2019-07-26 22:41:32 +0300

    sdsadsd

--- js/script.js
+++ js/script.js
@@ -11,11 +11,9 @@
    });
 
 
-   var element = document.createElement("div");
-   element.id = 'closeWindow';
-   var el = document.getElementById('closeWindow'); //
 
-      el.addEventListener("click", function(){
+
+       document.getElementById("closeWindow").addEventListener("click", function(){
          document.getElementsByClassName('container_overlay')[0].style.visibility = 'hidden';
 
        });
