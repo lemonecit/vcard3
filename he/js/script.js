1d14d0ef54f3b8b48cc6a97918ab39395414ecff
lemonec <pierre.lemonec@gmail.com>
2019-07-27 12:12:26 +0300

    RWERWER

---
+++ he/js/script.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+
+   document.getElementById("enable_socialLinks").addEventListener("click", function(){
+     document.getElementsByClassName('container_overlay')[0].style.visibility = 'visible';
+
+   });
